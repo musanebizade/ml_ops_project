049e92ad4882c979fb88d3a75aa07c4c890e3724
musanebizade <140913170+musanebizade@users.noreply.github.com>
2025-08-14 04:03:25 +0400

    Add files via upload

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
